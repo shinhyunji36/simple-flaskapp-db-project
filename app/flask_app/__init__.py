f1e1134d017dac6962d95c1fc7539b6081b7cbd4
shinhyunji36 <74661937+shinhyunji36@users.noreply.github.com>
2021-12-15 16:13:22 +0900

    Update __init__.py

--- app/flask_app/__init__.py
+++ app/flask_app/__init__.py
@@ -9,7 +9,7 @@ migrate = Migrate()
 def create_app():
     app = Flask(__name__)
 
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://secret'
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
     db.init_app(app)
